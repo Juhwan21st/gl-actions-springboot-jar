88f90c7926cb97ffe3f76cd6d89006d55a522c7d
Juhwan21st <jseo9123@conestogac.on.ca>
2026-01-29 19:36:28 -0500

    feat: Add CI/CD pipeline with GitHub Actions

---
+++ src/main/java/com/example/github_action/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.github_action.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){ return "Hello World V3"; }
+}
